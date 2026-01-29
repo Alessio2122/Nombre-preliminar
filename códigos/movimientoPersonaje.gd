917e0b9418b4af26ecb512c1f559c041b0af4307
Alessio2122 <alessio2122@alessio2122mc.com>
2026-01-29 17:56:01 +0100

    2026/01/29 17:55
    Se cambian las posiciones de distintos objetos.

--- códigos/movimientoPersonaje.gd
+++ códigos/movimientoPersonaje.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # Velocidad de movimiento en píxeles por segundo
-@export var speed : float = 300.0
+@export var speed : float = 600.0
 
 func _physics_process(_delta):
 	# Dirección horizontal (-1 a 1)
@@ -11,13 +11,13 @@ func _physics_process(_delta):
 	
 	# Movimiento horizontal
 	if dir_x != 0:
-		velocity.x = dir_x * 2 * speed
+		velocity.x = dir_x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
 	# Movimiento vertical
 	if dir_y != 0:
-		velocity.y = dir_y * 2 * speed
+		velocity.y = dir_y * speed
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
 	
